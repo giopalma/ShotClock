09b1942944a30a3c8a9d3a5fce37ed1967fca7a7
Giovanni Palma <giovanni.palma02@libero.it>
2024-11-17 11:02:22 +0100

    Rimozione del file perchè inutilizzato al momento

--- utils.py
+++
@@ -1,8 +0,0 @@
-import logging
-import os
-
-def get_mask():
-    mask_path = '.\\local\\mask.png'
-    if not os.path.exists(mask_path):
-        logging.error("Impossibile trovare il file {}".format(mask_path))
-        return False
