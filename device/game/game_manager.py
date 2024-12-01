ca963e6cc25282752672150ca8df9cf68c35bce9
Giovanni Palma <nannipa02@gmail.com>
2024-12-01 08:30:06 +0100

    Refactoring GameManager

--- device/game/game_manager.py
+++ device/game/game_manager.py
@@ -1,14 +1,3 @@
 from .game import Game
 
-
-class GameManager:
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super(GameManager, cls).__new__(cls, *args, **kwargs)
-            cls.game = None
-        return cls._instance
-
-    def get_game(self) -> Game:
-        return self.game
+game = None
