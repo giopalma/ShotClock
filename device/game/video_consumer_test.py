54c407c91b1db12fea67a686f8bbdeb2c4cf2e7f
Giovanni Palma <giovanni.palma02@libero.it>
2025-02-20 17:29:53 +0100

    modifiche e creazione dei test per video consumer

---
+++ device/game/video_consumer_test.py
@@ -0,0 +1,31 @@
+import unittest
+from ..table import TablePreset
+from .video_consumer import VideoConsumer
+from ..utils import hex_to_opencv_hsv
+
+
+class VideoConsumerTest(unittest.TestCase):
+
+    def setUp(self):
+        colors = ["#45c6ed", "#2288b5", "#1978a2", "#3bbbf3", "#0b6d9e"]
+        self.table = TablePreset(
+            id=0,
+            name="test_table_preset",
+            points=[(120, 80), (520, 80), (520, 280), (120, 280)],
+            table_colors=[hex_to_opencv_hsv(color) for color in colors],
+        )
+
+    def start_movement_callback(self):
+        print("START MOVEMENT")
+
+    def stop_movement_callback(self):
+        print("STOP MOVEMENT")
+
+    def test_init_video_consumer(self):
+        video_consumer = VideoConsumer(
+            self.table, self.start_movement_callback, self.stop_movement_callback
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
