4ccc238fad261e3f7e20cf7b8f761da4cca8c185
Giovanni Palma <giovanni.palma02@libero.it>
2025-03-18 20:21:16 +0100

    aggiunta incrementTime e getFrameMaskedUrl

--- web/src/api.js
+++ web/src/api.js
@@ -31,6 +31,13 @@ export async function pauseGame() {
     return gameAction('pause');
 }
 
+export async function incrementTime(player) {
+    const action = "increment_time_p" + player
+    const result = gameAction(action)
+    useGameStore().fetchGame()
+    return result
+}
+
 export async function resumeGame() {
     return gameAction('resume');
 }
@@ -54,5 +61,31 @@ export async function getFrameUrl() {
     } catch (error) {
         console.error("Errore durante il recupero del frame:", error);
         return null; // Restituisci null in caso di errore
+    }
+}
+
+export async function getFrameMaskedUrl(points, colors) {
+    const data = {
+        "points": points,
+        "colors": colors
+    }
+    try {
+        const response = await fetch('/api/video/frame', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        if (response.status === 200) {
+            const blob = await response.blob();
+            return URL.createObjectURL(blob);
+        } else {
+            console.error('Error:', response.statusText);
+            return false;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return false;
     }
 }
