cc29c960da73ca54572ac83446369068184338e0
Giovanni Palma <giovanni.palma02@libero.it>
2025-03-18 20:22:26 +0100

    Utilizzo di tabelle per mostrare i preset

--- web/src/components/SettingsComponent.vue
+++ web/src/components/SettingsComponent.vue
@@ -1,31 +1,62 @@
 <script setup>
-import { Panel, Select, Button, Divider } from 'primevue';
+import { Panel, ConfirmPopup, Button, Divider, DataTable, Column, useConfirm } from 'primevue';
 import { useSettingsStore } from '../store';
 import { ref, onMounted } from 'vue';
 import NewTablePresetModal from './NewTablePresetModal.vue';
+import NewRulesetModal from './NewRulesetModal.vue';
 
 const settingsStore = useSettingsStore()
-const selectedRuleset = ref(null)
-const selectedTablePreset = ref(null)
 
 const reloadSettings = async () => {
     settingsStore.fetchTablePresetsRulesets()
 }
 
 const addTablePreset = async () => {
-    console.log("Add Table Preset")
-    newTableDialogVisible.value = true
+    newTableModalVisible.value = true
+}
 
+const deleteTablePreset = async (id) => {
+    settingsStore.deleteTablePreset(id)
 }
 
 const addRuleset = async () => {
-    console.log("Add Ruleset")
+    newRulesetModalVisible.value = true
+}
+
+const deleteRuleset = async (id) => {
+    settingsStore.deleteRuleset(id)
+}
+
+const confirm = useConfirm()
+
+const showConfirm = (event, type, id) => {
+    confirm.require({
+        target: event.currentTarget,
+        message: 'Sei sicuro di voler eliminare l\'elemento?',
+        icon: 'pi pi-exclamation-triangle',
+        acceptLabel: "Sì",
+        acceptProps: {
+            severity: "danger"
+        },
+        rejectLabel: "No",
+        rejectProps: {
+            severity: "secondary"
+        },
+        accept: () => {
+            if (type === 'table') {
+                deleteTablePreset(id)
+            } else if (type === 'ruleset') {
+                deleteRuleset(id)
+            }
+        },
+    });
 }
 
 onMounted(() => {
     reloadSettings();
 });
-const newTableDialogVisible = ref(false)
+const newTableModalVisible = ref(false)
+const newRulesetModalVisible = ref(false)
 </script>
 <template>
     <Panel header="Settings Panel">
@@ -33,27 +64,40 @@ const newTableDialogVisible = ref(false)
             <Button icon="pi pi-refresh" severity="secondary" @click="reloadSettings" rounded text />
         </template>
         <div class=" flex flex-col w-md">
-            <Select id="tablepreset" ref="tablepresetref" class="flex-auto" autocomplete="off"
-                v-model="selectedTablePreset" :options="settingsStore.tableprests" optionLabel="name" showClear
-                placeholder="Table Preset">
-                <template #footer>
-                    <div class="p-3">
-                        <Button @click="addTablePreset" label="Aggiungi" fluid severity="secondary" text size="small"
-                            icon="pi pi-plus" />
+            <DataTable :value="settingsStore.tableprests" tableStyle="max-width: 50rem" size="small" stripedRows>
+                <template #header>
+                    <div class="flex flex-wrap items-center justify-between gap-2">
+                        <span class="text font-bold">Table Preset</span>
+                        <Button @click="addTablePreset" icon="pi pi-plus" rounded text severity="secondary" />
                     </div>
                 </template>
-            </Select>
+                <Column field="name" header="Name"></Column>
+                <Column field="id" header="" bodyStyle="text-align: right;">
+                    <template #body="slotProps">
+                        <ConfirmPopup></ConfirmPopup>
+                        <Button @click="showConfirm($event, 'table', slotProps.data.id)" label="Elimina" text
+                            severity="danger" />
+                    </template>
+                </Column>
+            </DataTable>
             <Divider />
-            <Select id="ruleset" class="flex-auto" autocomplete="off" v-model="selectedRuleset"
-                :options="settingsStore.rulesets" optionLabel="name" showClear placeholder="Ruleset">
-                <template #footer>
-                    <div class="p-3">
-                        <Button @click="addRuleset" label="Aggiungi" fluid severity="secondary" text size="small"
-                            icon="pi pi-plus" />
+            <DataTable :value="settingsStore.rulesets" tableStyle="max-width: 50rem" size="small" stripedRows>
+                <template #header>
+                    <div class="flex flex-wrap items-center justify-between gap-2">
+                        <span class="text font-bold">Ruleset</span>
+                        <Button @click="addRuleset" icon="pi pi-plus" rounded text severity="secondary" />
                     </div>
                 </template>
-            </Select>
+                <Column field="name" header="Name"></Column>
+                <Column field="id" header="" bodyStyle="text-align: right;">
+                    <template #body="slotProps">
+                        <Button @click="showConfirm($event, 'ruleset', slotProps.data.id)" label="Elimina" text
+                            severity="danger" />
+                    </template>
+                </Column>
+            </DataTable>
         </div>
-        <NewTablePresetModal v-model:visible="newTableDialogVisible" />
+        <NewTablePresetModal v-model:visible="newTableModalVisible" />
+        <NewRulesetModal v-model:visible="newRulesetModalVisible" />
     </Panel>
 </template>
