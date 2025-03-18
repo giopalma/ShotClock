afd4e31da44117b2c54d81b60d896574f891e6e6
Giovanni Palma <giovanni.palma02@libero.it>
2025-03-18 20:21:28 +0100

    ConfirmationService per ConfirmationPopup

--- web/src/main.js
+++ web/src/main.js
@@ -4,10 +4,12 @@ import { createPinia } from 'pinia'
 import { router } from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura';
+import ConfirmationService from 'primevue/confirmationservice';
 
 import App from './App.vue'
 import 'primeicons/primeicons.css'
 import './styles.css'
+
 const pinia = createPinia()
 const app = createApp(App)
 
@@ -18,4 +20,5 @@ app.use(PrimeVue, {
         preset: Aura
     }
 })
+app.use(ConfirmationService)
 app.mount('#app')
